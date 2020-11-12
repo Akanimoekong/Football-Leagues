0a54bb80ca3286837bf4a171e8541179840ad30a
Akanimoekong <akanimoekong@gmail.com>
2020-11-12 15:29:41 +0100

    Part of the Work

---
+++ app/src/main/java/com/africinnovate/footballleagues/LaligaLeague.kt
@@ -0,0 +1,11 @@
+package com.africinnovate.footballleagues
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LaligaLeague : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_laliga_league)
+    }
+}
--- app/src/main/java/com/africinnovate/footballleagues/Selection.kt
+++ app/src/main/java/com/africinnovate/footballleagues/Selection.kt
@@ -1,11 +1,19 @@
 package com.africinnovate.footballleagues
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Selection : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selection)
+
+        val laligabtn = findViewById<Button>(R.id.btn_laliga)
+        laligabtn.setOnClickListener {
+            val intent = Intent(this, LaligaLeague::class.java)
+            startActivity(intent)
+        }
     }
 }
